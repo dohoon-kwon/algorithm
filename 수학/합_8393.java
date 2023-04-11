ba89ba92af7e11093281eeabf7fa50d2b02e9757
DoHoon Kwon <56423448+dohoon-kwon@users.noreply.github.com>
2023-04-11 09:46:15 +0900

    no message

---
+++ 수학/합_8393.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+        
+        int result = 0;
+        
+        for(int i = 1; i <= n; i++) {
+            result += i;
+        }
+        
+        System.out.print(result);
+	}
+}
